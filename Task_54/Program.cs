0d1e5a63b8946eb683b57fa5a2cd8313bc8575c2
c8ler <kotovsergey@yandex.ru>
2022-06-09 05:59:33 +0300

    task 54 done

--- Task_54/Program.cs
+++ Task_54/Program.cs
@@ -1,8 +1,6 @@
 ﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая
 // упорядочит по убыванию элементы каждой строки двумерного массива.
 
-// Не удалось понять как сделать, чтобы все строки сортировались до конца =(
-
 bool run = true;
 while (run)
 {
@@ -53,11 +51,11 @@ void PrintArray(int[,] array)
 int[,] SortArr(int[,] array)
 {
     int temp = 0;
-    for (int i = 0; i < array.GetLength(0); i++)
+    for (int i = 0; i < array.GetLength(0) * array.GetLength(1); i++)
     {
         for (int j = 0; j < array.GetLength(0); j++)
         {
-            for (int k = 0; k < array.GetLength(1) - j - 1; k++)
+            for (int k = 0; k < array.GetLength(1)- 1; k++)
             {
                 if (array[j, k + 1] > array[j, k])
                 {
